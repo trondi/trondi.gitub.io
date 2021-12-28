3a4e7dfaebada02a34ba0e36d4f00d9f3df63116
ericabyss <trond746@gmail.com>
2021-12-29 02:05:32 +0900

    emotionJS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import React, { FunctionComponent } from 'react'
-import Text from 'components/Text'
 import { Link } from 'gatsby'
 
 const IndexPage: FunctionComponent = function () {
   return (
       <div>
-          <Text text="Hello, World!" />
           <a href="/info/">To Info</a>
       </div>
       
