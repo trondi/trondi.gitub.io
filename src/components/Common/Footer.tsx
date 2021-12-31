2d3e251c68c3a7da76bfa1c09cd0dc9ef5613ff9
ericabyss <trond746@gmail.com>
2021-12-31 23:55:39 +0900

    _

---
+++ src/components/Common/Footer.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+const FooterWrapper = styled.div`
+    display: grid;
+    place-items: center;
+    margin-top: auto;
+    padding: 50px;
+    font-size: 15px;
+    text-align: center;
+    line-height: 1.5;
+`
+
+const Footer: FunctionComponent = function () {
+    return (
+        <FooterWrapper>
+            Thnk u 4 visiting my blog, have a good day 🥰
+        </FooterWrapper>
+    )
+}
